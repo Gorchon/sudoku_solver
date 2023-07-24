321481df650af5c8a1532c61c57c91a5db513c7e
Gorchon <A01254831@tec.mx>
2023-07-23 23:40:02 -0700

    sudoku_3rd

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
 import pyautogui
+import os
+from SudokuSolver import *
+
 def main():
     # Make sure alt-tabbing switches to the browser where sudoku.com is open
     pyautogui.hotkey("alt", "tab", interval=0.1)
@@ -13,10 +16,16 @@ def main():
     preprocessed = preprocess(screenshot)
     square_contour = find_sudoku_contour(preprocessed)
     cropped_grid = crop_grid(screenshot, square_contour)
-    cv2.imshow("sudoku", cropped_grid)    
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-    
+     # Split the sudoku into 81 squares and detect the digits
+    squares_images = split_grid(cropped_grid)
+    sudoku = squares_images_to_sudoku(squares_images)
+
+    # Print the Sudoku grid as a matrix
+    print(sudoku)
+    solver = SudokuSolver(sudoku)
+    solved = solver.solve()
+    print(solved)
+    solve_on_website(square_contour, solved)    
     
 # -- Image preprocessing
 def preprocess(screenshot):
@@ -48,8 +57,55 @@ def crop_grid(screenshot, square):
     return cropped
 
 
+def split_grid(cropped_grid):
+    # Create the squares directory if it doesn't exist
+    if not os.path.exists('squares'):
+        os.makedirs('squares')
 
+    img = preprocess(cropped_grid)
+    img = skimage.segmentation.clear_border(img)
+    img = 255 - img
+    height, _ = img.shape
+    square_size = height // 9
+    squares = []
+    for i in range(9):
+        for j in range(9):
+            square = img[i*square_size:(i+1)*square_size, j*square_size:(j+1)*square_size]
+            squares.append(square)
+            # Save the square as an image file in the squares directory
+            cv2.imwrite(f'squares/square_{i}_{j}.png', square)
+         
+    return squares
 
+# -- Machine learning model
+def squares_images_to_sudoku(squares_images):
+    knn = create_knn_model()
+    sudoku = np.zeros((81), dtype=int)
+    for i, image in enumerate(squares_images):
+        sudoku[i] = predict_digit(image, knn)
+    return sudoku.reshape(9, 9)
 
+def predict_digit(img, knn):
+    img = img.reshape(1, -1)
+    return knn.predict(img)[0]
+
+def create_knn_model():
+    df = pd.read_csv("dataset.csv")
+    X = df.iloc[:, :-1].values
+    y = df.iloc[:, -1].values
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X, y)
+    return knn
+
+        
+# -- Sending the solution to the website
+def solve_on_website(square_contour, solved):
+    x, y, w, h = cv2.boundingRect(square_contour)
+    square_size = h // 9
+    for i in range(9):
+        for j in range(9):
+            pyautogui.click(x + j*square_size + square_size//2, y + i*square_size + square_size//2)
+            pyautogui.press(str(solved[i, j]))
+            
 if __name__ == '__main__':
     main()
